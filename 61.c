e51f91ecc948e9477b506d6a98f1ed335e8ee4f3
roberto montalti <rmontalti@technogym.com>
2023-08-22 18:15:09 +0200

    Problem 61 part

---
+++ 61.c
@@ -0,0 +1,19 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE 0x1
+#define FALSE 0x0
+
+typedef unsigned char b8;
+typedef unsigned char u8;
+typedef unsigned int u32;
+typedef unsigned long long int u64;
+typedef int i32;
+
+#define NGON(k, n) ((n*((k-2)*n-(k-4)))/2)
+
+i32 main( void ) {
+    return 0;
+}
+
