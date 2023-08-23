cac31d9f8e0cc263469f9dc613eda531a5464c8d
roberto montalti <rmontalti@technogym.com>
2023-08-23 15:26:25 +0200

    Problem 61

--- 61.c
+++ 61.c
@@ -13,27 +13,35 @@ typedef int i32;
 
 #define NGON(k, n) ((n*((k-2)*n-(k-4)))/2)
 
-b8 is_cyclic(u64 a, u64 b) {
+b8 binds(u64 a, u64 b) {
     u64 aa = a - ((a/100)*100);
     u64 bb = b / 100;
     return aa == bb;
 }
 
-b8 find_cyclic_gon(u64 k, u64 *result, u64 wanted, u64 n) {
-    if (wanted == n) return TRUE;
+b8 find_cycles(u64 *result, u64 rlen, u64 *exclude, u64 elen, u64 n) {
+    if (rlen == n) return TRUE;
 
-    u64 v = NGON(k, 0);
-    u64 i = 0;
-    for (;v<999; (i++, v=NGON(k, i)));
-    for (;v<10000; (i++, v=NGON(k, i))) {
-        // search here for (length = 4) cyclics
-        if (n>0 && !is_cyclic(result[n-1], v)) continue;
-        if (wanted-1 == n && !is_cyclic(result[n-1], v) && !is_cyclic(v, result[0])) continue;
+    for (u64 try_k=3; try_k<elen; try_k++) {
+        if (exclude[try_k]) continue;
 
-        result[n] = v;
-        if (!find_cyclic_gon(k+1, result, wanted, n+1)) continue;
+        u64 i = 0;
+        u64 v = NGON(try_k, 0);
 
-        return TRUE;
+        // reach above 1000 to get in range [1000..9999]
+        for (;v<999; (i++, v=NGON(try_k, i)));
+
+        // recursion area, restore exclude[try_k]
+        exclude[try_k] = 1; // exclude try_k-gon for child calls as we are using it here
+        for (;v<10000; (i++, v=NGON(try_k, i))) {
+            if (n+1==rlen && !binds(v, result[0])) continue; // if last number does bind the first one, skip
+            else if (n>0 && !binds(result[n-1], v)) continue; // if current does not bind the previous one, skip
+
+            result[n] = v; // one of the above did actually bind, go on with the others
+            if (!find_cycles(result, rlen, exclude, elen, n+1)) continue;
+            else return TRUE;
+        }
+        exclude[try_k] = 0; // no good bind was found, backtrace try_k and let it free!
     }
 
     return FALSE;
@@ -47,7 +55,9 @@ u64 sum(u64 *a, u64 len) {
 
 i32 main( void ) {
     u64 result[6] = { 0 };
-    find_cyclic_gon(3, result, 3, 0);
+    u64 exclude[9] = { 0 };
+    find_cycles(result, 6, exclude, 9, 0);
+
     printf("result[0] = %lld\n", result[0]);
     printf("result[1] = %lld\n", result[1]);
     printf("result[2] = %lld\n", result[2]);
